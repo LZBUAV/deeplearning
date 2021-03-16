27dd463525d64c7f1b30add256853fc071261289
LZBUAV <1245039130@qq.com>
2021-03-16 22:23:46 +0800

    add the CNN network

--- lib/bbnet.py
+++ lib/bbnet.py
@@ -1,7 +1,4 @@
 from functools import reduce
-from operator import le
-from os import error
-from lib import activate_function
 import random
 import numpy as np
 
@@ -10,7 +7,7 @@ import numpy as np
 class Perceptioin(object):
     def __init__(self, input_numder, activate_fun):
         self.activate_fun = activate_fun
-        self.weigths = [0.0 for i in range(input_numder)]
+        self.weights = [0.0 for i in range(input_numder)]
         self.bias = 0.0
 
     def forward(self, input_vector):
@@ -18,14 +15,14 @@ class Perceptioin(object):
         return self.activate_fun(
             reduce(
                 lambda a,b : a+b, list(map(
-                    lambda a : a[0] * a[1], list(zip(input_vector, self.weigths)))
+                    lambda a : a[0] * a[1], list(zip(input_vector, self.weights)))
                 )
             , 0.0) + self.bias
         )
     def update(self, input_vector, output, label, learning_rate):
         error = label - output
-        self.weigths = list(map(
-            lambda x: x[1] + learning_rate * error * x[0], list(zip(input_vector, self.weigths))
+        self.weights = list(map(
+            lambda x: x[1] + learning_rate * error * x[0], list(zip(input_vector, self.weights))
         ))
         self.bias = self.bias + learning_rate * error
 
@@ -41,7 +38,7 @@ class Perceptioin(object):
             print("epoch-----", i + 1)
 
     def __str__(self) -> str:
-        return "weigths : %s\nbias : %s\n" % (self.weigths, self.bias)
+        return "weights : %s\nbias : %s\n" % (self.weights, self.bias)
 
 #-----------------------------------------------------Liner Unit------------------------------------------------------------
 #single liner unit, using the liner function as the activate function
@@ -151,7 +148,7 @@ class Connections(object):
     def add_connectioin(self, conn):
         self.connections.append(conn)
 
-class Network(object):
+class Fully_Connected_Network(object):
     def __init__(self, layers, activate_function) -> None:
         self.layers = []
         self.activate_function = activate_function
@@ -226,7 +223,7 @@ class Network(object):
             print("epoch :", i + 1, "finished!")
 
 #实现2 向量化编程,能够显著提高计算速度,几百倍
-class FullyConnect(object):
+class Fully_Connection_Layer(object):
     def __init__(self, input_size, output_size, activate_function):
         self.input_size = input_size
         self.output_size = output_size
@@ -248,11 +245,11 @@ class FullyConnect(object):
         self.weights = self.weights + learning_rate* self.weights_grad
         self.bias = self.bias + learning_rate * self.bias_grad
 
-class Network_Vector(object):
+class Fully_Connected_Network_Vector(object):
     def __init__(self, layers, activate_function) -> None:
         self.layers = []
         for i in range(len(layers) - 1):
-            self.layers.append(FullyConnect(layers[i], layers[i + 1], activate_function))
+            self.layers.append(Fully_Connection_Layer(layers[i], layers[i + 1], activate_function))
 
     def predict(self, sample):
         output = sample
@@ -284,3 +281,198 @@ class Network_Vector(object):
             for j in range(len(samples)):
                 loss = self.train_once(lables[j], samples[j], learning_rate)
             print("epoch :", i + 1, "|  loss :", loss)
+
+#-----------------------------------------------CNN network------------------------------------------------------
+def padding(input, zp):
+    if zp == 0:
+        return input
+    else:
+        if input.ndim == 3:
+            input_depth = input.shape[0]
+            input_height = input.shape[1]
+            input_width = input.shape[2]
+            padding_array = np.zeros((input_depth, input_height + 2*zp, input_width + zp +zp))
+            padding_array[:, zp : zp + input_height, zp : zp + input_width] = input
+            return padding_array
+        elif input.ndim == 2:
+            input_height = input.shape[0]
+            input_width = input.shape[1]
+            padding_array = np.zeros(input_height + 2*zp, input_width + 2*zp)
+            padding_array[zp : zp + input_height, zp + input_width] = input
+            return padding_array
+
+def get_patch(input, i, j, fliter_width, fliter_height, stride):
+    start_i = i * stride
+    start_j = j * stride
+    if input.ndim == 2:
+        return input[start_i : start_i + fliter_height, start_j : start_j + fliter_width]
+    elif input.ndim == 3:
+        return input[:, start_i : start_i + fliter_height, start_j : start_j + fliter_width]
+
+def conv(input, weights, output, stride, bias):
+    fliter_width = weights.shape[-1]
+    fliter_height = weights.shape[-2]
+    output_height = output.shape[0]
+    output_width = output.shape[1]
+    for i in range(output_height):
+        for j in range(output_width):
+            output[i][j] = (get_patch(input, i, j, fliter_width, fliter_height, stride) * weights).sum() + bias
+
+
+
+class Fliter(object):
+    def __init__(self, fliter_width, fliter_height, fliter_depth) -> None:
+        self.weights = np.random.uniform(-1e-4, 1e-4, (fliter_depth, fliter_height, fliter_width))
+        self.bias = 0
+        self.weights_grad = np.zeros(self.weights.shape)
+        self.bias_grad = 0
+
+    def get_weights(self):
+        return self.weights
+
+    def get_bias(self):
+        return self.bias
+
+    def update(self, learning_rate):
+        self.weights = self.weights - learning_rate*self.weights_grad
+        self.bias = self.bias - learning_rate*self.bias_grad
+
+def element_wise_op(output, activate_function):
+    for i in np.nditer(output, op_flags=["readwrite"]):
+        i[...] = activate_function(i) 
+
+class Convolution_Layer(object):
+    def __init__(self, input_width, input_height, input_depth, fliter_width, fliter_height, fliter_number, zero_padding, stride, activate_function, learning_rate) -> None:
+        self.input_height = input_height
+        self.input_width = input_width
+        self.channel_number = input_depth
+        self.fliter_height = fliter_height
+        self.fliter_width = fliter_width
+        self.fliter_number = fliter_number
+        self.zero_padding = zero_padding
+        self.stride = stride
+        self.output_heigth = self.calculate_output_size(self.input_height, self.fliter_height, self.zero_padding, self.stride)
+        self.output_width = self.calculate_output_size(self.input_width, self.fliter_width, self.zero_padding, self.stride)
+        self.output = np.zeros((self.fliter_number, self.output_heigth, self.output_width))
+        self.fliters = []
+        for i in range(self.fliter_number):
+            self.fliters.append(Fliter(self.fliter_width, self.fliter_height, self.channel_number))
+        self.activate_function = activate_function
+        self.learning_rate = learning_rate
+
+    def calculate_output_size(self, input_size, fliter_size, zero_padding, stride):
+        return (input_size + 2*zero_padding -fliter_size) // stride + 1
+
+    def forward(self, input):
+        self.input = input
+        self.padded_input = padding(self.input, self.zero_padding)
+        for i in range(self.fliter_number):
+            weights = self.fliters[i].get_weights()
+            bias = self.fliters[i].get_bias()
+            conv(self.padded_input, weights, self.output[i], self.stride, bias)
+        element_wise_op(self.output, self.activate_function.relu_forward)
+
+    def expand_sensitivity_map(self, sensitivity_array):
+        depth = sensitivity_array.shape[0]
+        expanded_width = (self.input_width - self.fliter_width + 2 * self.zero_padding + 1)
+        expanded_height = (self.input_height - self.fliter_height + 2 * self.zero_padding + 1)
+        expand_array = np.zeros((depth, expanded_height, expanded_width))
+        for i in range(self.output_heigth):
+            for j in range(self.output_width):
+                expand_array[:, i * self.stride, j * self.stride] = sensitivity_array[:, i, j]
+        return expand_array
+
+    def create_delta_array(self):
+        return np.zeros((self.channel_number,
+            self.input_height, self.input_width))
+
+    def bp_sensitivity(self, sensitivity_map, activate_function):
+        expand_sensitivity_map = self.expand_sensitivity_map(sensitivity_map)
+        expand_width = expand_sensitivity_map.shape[2]
+        zp = (self.input_width + self.fliter_width -1 - expand_width) // 2
+        padding_expand_sensitivity_map = padding(expand_sensitivity_map, zp)
+        self.delta_array = self.create_delta_array()
+        for f in range(len(self.fliters)):
+            fliter = self.fliters[f]
+            fliter_weights = np.array(list(map(lambda i : np.rot90(i, 2), fliter.get_weights())))
+            delta_array = self.create_delta_array()
+            for d in range(delta_array.shape[0]):
+                conv(padding_expand_sensitivity_map[f], fliter_weights, delta_array[d], 1, 0)
+            self.delta_array += delta_array
+        derivative_array = np.array(self.input)
+        element_wise_op(derivative_array, activate_function.relu_backward)
+        self.delta_array = self.delta_array * derivative_array
+
+    def cacl_grad(self, sensitivity_map):
+        expanded_array = self.expand_sensitivity_map(sensitivity_map)
+        for f in range(self.fliter_number):
+            # 计算每个权重的梯度
+            fliter = self.fliters[f]
+            for d in range(fliter.weights.shape[0]):
+                conv(self.padded_input[d], 
+                     expanded_array[f],
+                     fliter.weights_grad[d], 1, 0)
+            # 计算偏置项的梯度
+            fliter.bias_grad = expanded_array[f].sum()
+
+    def backward(self, sensitivity_map, activate_function):
+        self.bp_sensitivity(sensitivity_map, activate_function)
+        self.cacl_grad(sensitivity_map)
+
+    def update(self):
+        for flayer in self.fliters:
+            flayer.update(self.learning_rate)
+
+def get_max_index(array):
+    max_i = 0
+    max_j = 0
+    max_value = array[0,0]
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            if array[i,j] > max_value:
+                max_value = array[i,j]
+                max_i, max_j = i, j
+    return max_i, max_j
+
+class MaxPoolingLayer(object):
+    def __init__(self, input_width, input_height, 
+                 channel_number, filter_width, 
+                 filter_height, stride):
+        self.input_width = input_width
+        self.input_height = input_height
+        self.channel_number = channel_number
+        self.filter_width = filter_width
+        self.filter_height = filter_height
+        self.stride = stride
+        self.output_width = (input_width - 
+            filter_width) // self.stride + 1
+        self.output_height = (input_height -
+            filter_height) // self.stride + 1
+        self.output_array = np.zeros((self.channel_number,
+            self.output_height, self.output_width))
+
+    def forward(self, input_array):
+        for d in range(self.channel_number):
+            for i in range(self.output_height):
+                for j in range(self.output_width):
+                    self.output_array[d,i,j] = (    
+                        get_patch(input_array[d], i, j,
+                            self.filter_width, 
+                            self.filter_height, 
+                            self.stride).max())
+
+    def backward(self, input_array, sensitivity_array):
+        self.delta_array = np.zeros(input_array.shape)
+        for d in range(self.channel_number):
+            for i in range(self.output_height):
+                for j in range(self.output_width):
+                    patch_array = get_patch(
+                        input_array[d], i, j,
+                        self.filter_width, 
+                        self.filter_height, 
+                        self.stride)
+                    k, l = get_max_index(patch_array)
+                    self.delta_array[d, 
+                        i * self.stride + k, 
+                        j * self.stride + l] = \
+                        sensitivity_array[d,i,j]
